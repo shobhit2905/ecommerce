016c3e03b7d0492f6e921dfdc9111451639763bf
shobhit2905 <31738575+shobhit2905@users.noreply.github.com>
2017-11-04 17:13:08 +0530

    Add files via upload

---
+++ paypal_cancel.php
@@ -0,0 +1,26 @@
+
+
+<html>
+	<head>
+		<title>Payment Cancel!</title>
+	</head>
+
+<body>
+
+	<h2>Payment was cancel</h2>
+	<h3> Your payment was not successful,please go back to our shop and try again</h3>
+	<h3><a href="#">Go to back</a></h3>
+
+			
+				
+
+
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ paypal_success.php
@@ -0,0 +1,30 @@
+<?php 
+session_start(); 
+?>
+
+
+
+<html>
+	<head>
+		<title>Payment Successful!</title>
+	</head>
+
+<body>
+
+	<h2>Welcome <?php echo $_SESSION['customer_email']; ?></h2>
+	<h3> Your payment was successful,please go to your account</h3>
+	<h3><a href="#">Go to your account</a></h3>
+
+			
+				
+
+
+</body>
+</html>
+
+
+
+
+
+
+
